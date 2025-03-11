baf1b7782746193a0712306b5cb692fbc062cf6b
nikolay_dimitrov256 <raystlin7@gmail.com>
2025-03-11 21:19:28 +0200

    chore: Worked on the mobile menus

--- fashionShop/fashionShop/settings.py
+++ fashionShop/fashionShop/settings.py
@@ -184,7 +184,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('home')
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'bg'
 
 TIME_ZONE = 'EET'
 
