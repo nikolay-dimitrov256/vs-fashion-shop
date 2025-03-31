c0206904cc8b5e7f912a1a71c2d3459b3677f505
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-03-31 14:28:15 +0300

    fix: Optimizet queries in product page view

--- fashionShop/fashionShop/items/views.py
+++ fashionShop/fashionShop/items/views.py
@@ -1,4 +1,6 @@
-from django.db.models import OuterRef, Subquery
+from pprint import pprint
+
+from django.db.models import OuterRef, Subquery, Prefetch
 from django.shortcuts import render
 from django.views.generic import DetailView
 
@@ -11,8 +13,24 @@ class ItemDetailView(DetailView):
     template_name = 'items/single.html'
 
     def get_object(self, queryset=None):
-        item = super().get_object(queryset)
-        item.main_picture = item.pictures.first()
+        #item = super().get_object(queryset)
+        item = (
+            Item.objects
+            .select_related('category', 'sub_category', 'pattern', 'color_group')
+            .prefetch_related(
+                'sizes',
+                'linked_items',
+                'linked_items__pictures',
+                'pictures',
+                # Prefetch(
+                #     'pattern__items',
+                #     queryset=Item.objects.exclude(pk=self.kwargs['pk']),
+                #     to_attr='other_colors'
+                # )
+            )
+            .get(pk=self.kwargs['pk'])
+        )
+
         item.detail_pictures = item.pictures.filter(is_detail=True)
         if item.description:
             item.description = item.description.split(';')
@@ -26,6 +44,8 @@ class ItemDetailView(DetailView):
 
         context['other_colors'] = (
             Item.objects
+            .select_related('pattern')
+            .prefetch_related('pictures')
             .filter(pattern=self.object.pattern)
             .exclude(item_number=self.object.item_number)
         )
