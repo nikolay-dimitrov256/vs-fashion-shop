d589e1ac73498ecebbfcf1b77c0f7112d2bcd0ad
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-04-30 15:54:53 +0300

    fix: Fixed issue in store admin

--- fashionShop/fashionShop/stores/admin.py
+++ fashionShop/fashionShop/stores/admin.py
@@ -7,7 +7,7 @@ from fashionShop.stores.models import Store
 class AddressInline(admin.StackedInline):
     model = Address
     can_delete = False
-    fields = ['country', 'province', 'town', 'postal_code', 'street', 'number', 'building', 'entrance', 'apartment']
+    fields = ['country', 'province', 'city', 'postal_code', 'street', 'number', 'building', 'entrance', 'apartment']
 
 
 @admin.register(Store)
