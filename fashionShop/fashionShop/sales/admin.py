b9ff00ab83aed91a697dcaceb193fee47d7597de
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-05-19 10:43:00 +0300

    Made comment editable in Online Order admin

--- fashionShop/fashionShop/sales/admin.py
+++ fashionShop/fashionShop/sales/admin.py
@@ -15,7 +15,7 @@ class OrderItemInline(admin.TabularInline):
 class OnlineOrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
     readonly_fields = ['order_code', 'first_name', 'last_name', 'phone', 'user', 'email',
-                       'shipping_method', 'office', 'town', 'address', 'total', 'comment',
+                       'shipping_method', 'office', 'town', 'address', 'total',
                        'created_at', 'updated_at']
     search_fields = ['pk', 'order_code']
     list_filter = ['created_at']
