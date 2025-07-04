cf6f85d8686ab9491abd5c222a3de77a2197858c
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-07-04 15:37:09 +0300

    fix: Locked the SalesBySizeView for staff users

--- fashionShop/fashionShop/analytics/views.py
+++ fashionShop/fashionShop/analytics/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Sum
 from django.shortcuts import render
 from django.views.generic import TemplateView
@@ -5,7 +6,7 @@ from django.views.generic import TemplateView
 from fashionShop.items.models import Size
 
 
-class SalesBySizeView(TemplateView):
+class SalesBySizeView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = 'analytics/sales-by-size.html'
 
     def get_context_data(self, **kwargs):
@@ -14,3 +15,6 @@ class SalesBySizeView(TemplateView):
         context['sizes'] = Size.objects.annotate(sales=Sum('order_items__quantity')).order_by('size')
 
         return context
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.is_staff
