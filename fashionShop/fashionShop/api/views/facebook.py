4c19cdbdb08a65ed11754f05364991c8775f8190
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-08-12 16:42:16 +0300

    Updated Facebook feed

--- fashionShop/fashionShop/api/views/facebook.py
+++ fashionShop/fashionShop/api/views/facebook.py
@@ -63,6 +63,9 @@ def products_feed(request):
         SubElement(it, 'is_new').text = str(item.is_new)
         sales = item.order_items.count()
         SubElement(it, 'is_bestseller').text = str(sales >= 5)
+        SubElement(it, 'product_type').text = item.category.name_bg
+        SubElement(it, 'custom_label_1').text = str(item.is_new)
+        SubElement(it, 'custom_label_2').text = str(sales >= 5)
 
     xml_str = tostring(rss, encoding='utf-8')
     # pretty_xml = xml.dom.minidom.parseString(xml_str).toprettyxml(indent='  ')
