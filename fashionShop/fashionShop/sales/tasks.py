d4b9deb5b0e05f32c76177a11379a085d1846197
nikolay-dimitrov256 <raystlin7@gmail.com>
2025-12-15 16:11:11 +0200

    Stopped SMS sending

--- fashionShop/fashionShop/sales/tasks.py
+++ fashionShop/fashionShop/sales/tasks.py
@@ -70,6 +70,7 @@ def send_bisoft_report(order_id, save=True):
 
 @shared_task
 def send_sms(to: str, message: str | None) -> dict | None:
+    return None
     if not to or not message:
         return None
 
